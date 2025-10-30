e67b4b5d386cf7410c89287a50d0826f0685d636
erffy <ceyhunerenerguven.07@proton.me>
2025-10-30 21:27:53 +0300

    Major improvements

--- archeoniso/airootfs/root/.config/fish/README.md
+++
@@ -1,7 +0,0 @@
-## Erffy Dots - [Fish](https://github.com/fish-shell/fish-shell)
-My fish configuration files
-
-### Installation
-```sh
-bash -c "CONFIG_NAME=fish CONFIG_DIR_NAME=fish $(curl -fsSL https://raw.githubusercontent.com/erffy-dots/.generic/main/install_subconfig.sh)"
-```
--- archeoniso/profiledef.sh
+++ archeoniso/profiledef.sh
@@ -10,17 +10,19 @@ iso_application="Archeon Live/Rescue DVD"
 iso_version="${iso_date}"
 install_dir="arch"
 buildmodes=('iso')
-bootmodes=('bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
+bootmodes=('bios.syslinux' 'uefi.systemd-boot')
 arch="x86_64"
-iso_fullname="${iso_name}-${iso_date}-${arch}.iso"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'zstd' '-Xcompression-level' '22' '-b' '1M')
+bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
-  ["/etc/gshadow"]="0:0:400"
-  ["/etc/polkit-1/rules.d"]="0:0:750"
+  ["/etc/gshadow"]="0:0:0400"
   ["/root"]="0:0:750"
-  ["/usr/local/bin/switch-nm"]="0:0:755"
-  ["/usr/local/bin/fix-keyring"]="0:0:755"
+  ["/root/.automated_script.sh"]="0:0:755"
+  ["/root/.gnupg"]="0:0:700"
+  ["/usr/local/bin/choose-mirror"]="0:0:755"
+  ["/usr/local/bin/Installation_guide"]="0:0:755"
+  ["/usr/local/bin/livecd-sound"]="0:0:755"
 )
